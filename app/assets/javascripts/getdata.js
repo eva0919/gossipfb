d628a253f4a37627e790fb270304fc8be78c2476
Lucas <lucaskointw@gmail.com>
2014-02-23 06:38:22 +0800

    template changed

--- app/assets/javascripts/getdata.js
+++ app/assets/javascripts/getdata.js
@@ -3,15 +3,21 @@ function getTempData(){
 	var name_arr = [];
 	var id_arr =[];
 	var url_arr =[];
+  var data = [];
 	for (var i = 0; i < parseInt(count); i++) {
 		var text = ".temp_data_" + i ;
-		name_arr.push($(text).attr("enname") );
-		id_arr.push($(text).attr("enid") );
-		url_arr.push($(text).attr("enurl") );
+    data.push({"id": $(text).attr("enid"),
+              "name": $(text).attr("enname"),
+              "url" : $(text).attr("enurl")});
+		// name_arr.push($(text).attr("enname") );
+		// id_arr.push($(text).attr("enid") );
+		// url_arr.push($(text).attr("enurl") );
 	};
 	$("#temp").remove();
+
 	// friend list
 	console.log(url_arr);
 	console.log(name_arr);
 	console.log(id_arr);
+  return data;
 }
